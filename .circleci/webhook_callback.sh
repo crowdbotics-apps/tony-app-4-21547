2eaff3ed2454a278768542c9b3d8e74e524fada7
TAN DUONG <tanduong@live.com>
2020-10-16 15:31:15 -0500

    update

--- .circleci/webhook_callback.sh
+++ .circleci/webhook_callback.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Ignore iOS error if there is no paid plan
-if [ "$CIRCLE_JOB" == "ios" ] && [ "$HAS_PAID_PLAN" != 1 ]; then
-  echo "No paid plan, not calling webhook"
-  exit
-fi
-
 set -euo pipefail
 
 # Script based on https://altinukshini.wordpress.com/2019/01/09/circleci-notifications-in-rocketchat/
